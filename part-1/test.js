c7a9aa7ee6227afab7ca515b06216246bf960fa1
bterbs <brittanyterbush@gmail.com>
2017-10-10 16:17:38 -0700

    completed weekday tests, both now passing, start snippets test

--- part-1/test.js
+++ part-1/test.js
@@ -1,12 +1,27 @@
 const expect = require('chai').expect;
-const {weekday, snippet, numProps, filterBetween } = require('./functions.js');
+const { weekday, snippet, numProps, filterBetween } = require('./functions.js');
 
 // 10: A test using expected/valid inputs for the weekday() function is written in tests.js.
 // 10: A test using unexpected/invalid inputs for the weekday() function is written in tests.js.
 
 describe('weekday()', () => {
-  it('returns expected day with valid input', () => {
-    let date = new Date(2017, 9, 10);
+  it('returns correct day with valid input', () => {
+    const date = new Date(2017, 9, 10);
     expect(weekday(date)).to.equal('Tue');
+  });
+  it('returns undefined with invalid/unexpected input', () => {
+    const date = 'today';
+    expect(weekday(date)).to.equal(undefined);
+  });
+});
+
+describe('weekday()', () => {
+  it('returns expected day with valid input', () => {
+    const date = new Date(2017, 9, 10);
+    expect(weekday(date)).to.equal('Tue');
+  });
+  it('returns undefined with invalid/unexpected input', () => {
+    const date = 'today';
+    expect(weekday(date)).to.equal(undefined);
   });
 });
