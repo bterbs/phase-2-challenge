11de766fb2e790ed4f12f58c98e437c2adca1748
bterbs <brittanyterbush@gmail.com>
2017-10-11 10:13:22 -0700

    completed searchByState, need to select results to return still

---
+++ part-2/searchByState.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const clientsFile = './clients.json';
+const readFromFile = clientsFile => JSON.parse(fs.readFileSync(clientsFile), 'utf 8');
+
+const stateName = process.argv.slice(2).join();
+
+const searchByState = (stateName) => {
+  const clients = readFromFile(clientsFile);
+  let regexp = new RegExp(stateName, "gi");
+  const results = clients.filter((client) => {
+    return regexp.test(client.state)
+  });
+  console.log(`Finding clients in State "${stateName}"...`)
+  console.log(results);
+};
+
+searchByState(stateName);
