2415311788a2d1348ece97389bd59da0085b8ee6
bterbs <brittanyterbush@gmail.com>
2017-10-11 10:00:32 -0700

    all specs met except print format

--- part-2/searchByName.js
+++ part-2/searchByName.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const clientsFile = './clients.json';
-
 const readFromFile = clientsFile => JSON.parse(fs.readFileSync(clientsFile), 'utf 8');
 
 // searchByName.js takes one string argument (let's call it name), finds all the clients
@@ -8,14 +7,15 @@ const readFromFile = clientsFile => JSON.parse(fs.readFileSync(clientsFile), 'ut
 // id and rep_name of each matching client.
 // Your searches should not be case-sensitive: i.e. a search for "Bob" is the same as a
 // search for "bob".
+const name = process.argv.slice(2).join();
 
 const searchByName = (name) => {
   const clients = readFromFile(clientsFile);
-  var regexp = new RegExp('^' + name, "gi");
+  let regexp = new RegExp('^' + name, "gi");
   const results = clients.filter((client) => {
-    return regexp.test(client.rep_name);
-  })
+    return regexp.test(client.rep_name)
+  });
   console.log(results);
 };
 
-searchByName('Ad')
+searchByName(name)
